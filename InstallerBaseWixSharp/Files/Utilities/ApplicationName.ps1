2698e925824752258e2e1ffd48a16fb8ef6ebc54
VPKSoft <vpksoft@vpksoft.net>
2020-10-24 08:07:03 +0300

    beta rev.13.

--- InstallerBaseWixSharp/Files/Utilities/ApplicationName.ps1
+++ InstallerBaseWixSharp/Files/Utilities/ApplicationName.ps1
@@ -1,4 +1,28 @@
-﻿$files = Get-ChildItem -Recurse ..\..\* -Include "*.cs", "*.wxl" -Exclude "*.Designer.cs"
+﻿<#
+MIT License
+
+Copyright(c) 2020 Petteri Kautonen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+#>
+
+$files = Get-ChildItem -Recurse ..\..\* -Include "*.cs", "*.wxl" -Exclude "*.Designer.cs"
 
 # English (United States);Finnish (Finland)
 $languages = "0x00000102;0x00000118"; # see the Files\Localization\SupportedLanguages.cs file..
